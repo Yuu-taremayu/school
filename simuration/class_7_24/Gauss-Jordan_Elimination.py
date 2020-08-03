3bb6dcadf129618d6b4417b3d3007854d6aa1909
Yuu-taremayu <taremayu.yuu@gmail.com>
2020-08-03 14:42:41 +0900

    kadai

--- simuration/class_7_24/Gauss-Jordan_Elimination.py
+++ simuration/class_7_24/Gauss-Jordan_Elimination.py
@@ -9,6 +9,7 @@ def Gauss_Jordan_Elimination(X, N):
         # j repeats to column direction
         for j in range(k + 1, N + 1):
             X[k][j] = X[k][j] / X[k][k]
+            cnt += 1
 
             # i repeats to row direction
             # become 0 except [k][k]element
@@ -23,10 +24,10 @@ if __name__ == '__main__':
     #A = np.array([[2, 3, 4], [3, 5, 2], [4, 3, 30]], dtype=float)
     #B = np.array([[6], [5], [32]], dtype=float)
 
-    #A = pd.read_csv('A100.csv', header=None)
-    #B = pd.read_csv('b100.csv', header=None)
-    A = pd.read_csv('A1000.csv', header=None)
-    B = pd.read_csv('b1000.csv', header=None)
+    A = pd.read_csv('A100.csv', header=None)
+    B = pd.read_csv('b100.csv', header=None)
+    #A = pd.read_csv('A1000.csv', header=None)
+    #B = pd.read_csv('b1000.csv', header=None)
 
     # prepare matrix for calculation
     A = np.array(A)
--- simuration/class_7_24/Gaussian_Elimination.py
+++ simuration/class_7_24/Gaussian_Elimination.py
@@ -9,6 +9,7 @@ def Gaussian_Elimination(X, N):
 
         for j in range(k + 1, N + 1):
             X[k][j] = X[k][j] / X[k][k]
+            cnt += 1
 
             for i in range(k + 1, N):
                 X[i][j] = X[i][j] - X[k][j] * X[i][k]
@@ -17,6 +18,7 @@ def Gaussian_Elimination(X, N):
     x = np.zeros((1, N), dtype=float)
     for k in reversed(range(N)):
         x[0][k] = X[k][N]
+        cnt += 1
 
         for j in range(k + 1, N):
             x[0][k] = x[0][k] - X[k][j] * x[0][j]
@@ -26,10 +28,10 @@ def Gaussian_Elimination(X, N):
 
 if __name__ == '__main__':
     # read csv file
-    #A = pd.read_csv('A100.csv', header=None)
-    #B = pd.read_csv('b100.csv', header=None)
-    A = pd.read_csv('A1000.csv', header=None)
-    B = pd.read_csv('b1000.csv', header=None)
+    A = pd.read_csv('A100.csv', header=None)
+    B = pd.read_csv('b100.csv', header=None)
+    #A = pd.read_csv('A1000.csv', header=None)
+    #B = pd.read_csv('b1000.csv', header=None)
 
     # convert to ndarray from dataframe
     A = np.array(A)
